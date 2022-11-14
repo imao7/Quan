f33e3cf11f40d3dc28adda1497d52ade1c348e5e
imao7 <110957436+imao7@users.noreply.github.com>
2022-11-15 07:38:54 +0800

    Update naiyou.js

--- naiyou.js
+++ naiyou.js
@@ -1,18 +1,23 @@
+/******************************
+
+脚本功能：奶油壁纸解锁会员
+软件版本：2.0
+脚本作者：imao7
+更新时间：2022-11-15
+使用声明：⚠️此脚本仅供学习与交流，
+        请勿转载与贩卖！⚠️⚠️⚠️
+*******************************
 [rewrite_local]
 #奶由壁纸vip
-^https://nz-api.duitang.com/account/me url script-response-body https://raw.githubusercontent.com/imao7/Quan/master/naiyou.js
+https://nz-api.duitang.com/account/me url script-response-body https://raw.githubusercontent.com/imao7/Quan/master/naiyou.js
 [mitm]
 hostname = nz-api.duitang.com
 
-********************************/
-var body = $response.body;
-var url = $request.url;
-var obj = JSON.parse(body);
+*******************************
+var body = $response.body; 
+var obj = JSON.parse(body); 
 
-const vip = '/account/me';
+obj.data.vip = true
 
-if (url.indexOf(vip) != -1) {
-    obj.data.vip = true;
-   body = JSON.stringify(obj);
-}
+body = JSON.stringify(obj);
 $done({body});
