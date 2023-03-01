a79abb6406f10877d355a6d7d4802e6d26192a09
imao7 <110957436+imao7@users.noreply.github.com>
2023-03-02 07:29:08 +0800

    更新 nybz.js

--- nybz.js
+++ nybz.js
@@ -1,15 +1,2 @@
-[rewrite_local]
-#奶由壁纸vip
-https://nz-api.duitang.com/account/me url script-response-body https://raw.githubusercontent.com/imao7/Quan/master/nybz.js
-[mitm]
+^https?:\/\/nz-api\.duitang\.com\/account\/me url response-body "vip":false response-body "vip":true
 hostname = nz-api.duitang.com
-
-*****************************/
-
-var body = $response.body; 
-var obj = JSON.parse(body); 
-
-obj.data.vip = true
-
-body = JSON.stringify(obj);
-$done({body});
